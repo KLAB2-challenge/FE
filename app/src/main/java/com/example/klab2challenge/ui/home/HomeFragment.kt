0debdbcccae14557336833bdf15af0e949159eb8
nonaninona <starcraft0529@gmail.com>
2023-11-05 15:26:16 +0900

    feat : 사진 업로드 구현 ~ing

--- app/src/main/java/com/example/klab2challenge/ui/home/HomeFragment.kt
+++ app/src/main/java/com/example/klab2challenge/ui/home/HomeFragment.kt
@@ -1,10 +1,17 @@
 package com.example.klab2challenge.ui.home
 
+import android.Manifest
 import android.content.Intent
+import android.content.pm.PackageManager
+import android.os.Build
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.annotation.RequiresApi
+import androidx.core.app.ActivityCompat
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.navigation.fragment.findNavController
@@ -17,12 +24,11 @@ class HomeFragment : Fragment() {
 
     private var _binding: FragmentHomeBinding? = null
 
-    // This property is only valid between onCreateView and
-    // onDestroyView.
     private val binding get() = _binding!!
     private val popularViewModel = ChallengeViewModel()
     private val officialViewModel = ChallengeViewModel()
     private val userViewModel = ChallengeViewModel()
+
 
     override fun onCreateView(
         inflater: LayoutInflater,
