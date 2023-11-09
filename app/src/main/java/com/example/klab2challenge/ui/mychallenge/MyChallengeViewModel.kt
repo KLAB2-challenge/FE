22907308da67357761ee5c908dd95f5138a962e6
nonaninona <starcraft0529@gmail.com>
2023-11-09 17:52:17 +0900

    feat : my challenge list viewmodel, recycler adapter 적용

--- app/src/main/java/com/example/klab2challenge/ui/mychallenge/MyChallengeViewModel.kt
+++ app/src/main/java/com/example/klab2challenge/ui/mychallenge/MyChallengeViewModel.kt
@@ -3,11 +3,20 @@ package com.example.klab2challenge.ui.mychallenge
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.example.klab2challenge.retrofit.ChallengeContents
+import com.example.klab2challenge.retrofit.ChallengeInfos
+import com.example.klab2challenge.retrofit.GetChallengeResponse
 
 class MyChallengeViewModel : ViewModel() {
+    private var list = ArrayList<GetChallengeResponse>();
+    private var _itemList = MutableLiveData<ArrayList<GetChallengeResponse>>();
 
-    private val _text = MutableLiveData<String>().apply {
-        value = "This is dashboard Fragment"
+    val itemList: LiveData<ArrayList<GetChallengeResponse>> get() = _itemList
+
+
+    fun setMyChallenge(cList: ArrayList<GetChallengeResponse>) {
+        list = cList
+
+        _itemList.value = list
     }
-    val text: LiveData<String> = _text
 }
