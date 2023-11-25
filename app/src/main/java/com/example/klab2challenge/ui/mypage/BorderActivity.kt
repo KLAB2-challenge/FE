af1ad7811f29d369172e202cbad617d4964f4fe8
chrin05 <rinring105@gmail.com>
2023-11-26 05:53:26 +0900

    feat : Border xml 완료 및 기능 시작

--- app/src/main/java/com/example/klab2challenge/ui/mypage/BorderActivity.kt
+++ app/src/main/java/com/example/klab2challenge/ui/mypage/BorderActivity.kt
@@ -1,15 +1,27 @@
 package com.example.klab2challenge.ui.mypage
 
 import android.os.Bundle
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import com.example.klab2challenge.databinding.ActivityBorderBinding
 
 class BorderActivity : AppCompatActivity() {
     lateinit var binding : ActivityBorderBinding
+    private var isChecked = intArrayOf(0,0,0,0,0,0)
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityBorderBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        binding.cvBBackBtn.setOnClickListener {
+            finish()
+        }
+
+        binding.clBGoldContainer.setOnClickListener {
+            if (isChecked[0]==0) {
+                binding.ivBGoldCheck.visibility = View.VISIBLE
+            }
+        }
     }
 }
