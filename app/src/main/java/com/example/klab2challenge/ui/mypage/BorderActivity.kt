d7f5280b7405bb53829945f12bea39c59cf47568
chrin05 <rinring105@gmail.com>
2023-11-26 19:19:20 +0900

    feat : Border 기능 데이터 넘겨받는거 하는 중

--- app/src/main/java/com/example/klab2challenge/ui/mypage/BorderActivity.kt
+++ app/src/main/java/com/example/klab2challenge/ui/mypage/BorderActivity.kt
@@ -1,6 +1,7 @@
 package com.example.klab2challenge.ui.mypage
 
 import android.os.Bundle
+import android.util.Log
 import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import com.example.klab2challenge.R
@@ -11,6 +12,7 @@ class BorderActivity : AppCompatActivity() {
 
     private lateinit var border: ArrayList<View>
     private lateinit var icCheck: ArrayList<View>
+    private var borderCoin = arrayListOf(10,20,30,40,50,60)
     private var isChecked = arrayListOf(false, false, false, false, false, false)
     private var isUnlocked = arrayListOf(false, false, false, false, false, false)
     lateinit var color: ArrayList<Int>
@@ -20,14 +22,21 @@ class BorderActivity : AppCompatActivity() {
         setContentView(binding.root)
 
         color = arrayListOf(
-            getColor(R.color.gold), getColor(R.color.green), getColor(R.color.cherry),
-            getColor(R.color.blueberry), getColor(R.color.sunny), getColor(R.color.rainy)
+            getColor(R.color.gold),
+            getColor(R.color.green),
+            getColor(R.color.cherry),
+            getColor(R.color.blueberry),
+            getColor(R.color.sunny),
+            getColor(R.color.rainy)
         )
 
-
         border = arrayListOf(
-            binding.clBGoldContainer, binding.clBGreenContainer, binding.clBCherryContainer,
-            binding.clBBlueberryContainer, binding.clBSunnyContainer, binding.clBRainyContainer
+            binding.phBGold,
+            binding.phBGreen,
+            binding.phBCherry,
+            binding.phBBlueberry,
+            binding.phBSunny,
+            binding.phBRainy
         )
 
         icCheck = arrayListOf(
@@ -41,33 +50,69 @@ class BorderActivity : AppCompatActivity() {
 
         startClickListener() //setOnClickListener 추가
 
-
     }
 
     private fun startClickListener() {
-        for (i in 0..5 step (1)) {
+        Log.d("chaerin", "클릭")
+        for (i in 0..5){
             border[i].setOnClickListener {
-                if (!isChecked[i]) {
-                    icCheck[i].visibility = View.VISIBLE
-                    binding.ifbBProfileBorder.setBackgroundColor(color[i])
-                    isChecked[i] = true
-                } else {
-                    icCheck[i].visibility = View.GONE
-                    binding.ifbBProfileBorder.setBackgroundColor(getColor(R.color.WH))
-                    isChecked[i] = false
-                }
-
-                if (!isUnlocked[i]) {
-                    binding.tvBNoticeLabel.visibility = View.VISIBLE
-                    binding.tvBNoticeCoin.visibility = View.VISIBLE
-                    binding.tvBNoticeLabel2.visibility = View.VISIBLE
-                    binding.tvBNoticeCoin.setText("코인수")
-                } else {
-                    binding.tvBNoticeLabel.visibility = View.GONE
-                    binding.tvBNoticeCoin.visibility = View.GONE
-                    binding.tvBNoticeLabel2.visibility = View.GONE
-                }
+                borderChecking(i)
+                showNotice(i)
             }
+        }
+    }
+
+    private fun showNotice(i: Int) {
+        if (!isUnlocked[i]) {
+            binding.tvBNoticeLabel.visibility = View.VISIBLE
+            binding.tvBNoticeLabel2.visibility = View.VISIBLE
+            binding.tvBNoticeCoin.text = borderCoin[i].toString()
+            binding.tvBNoticeCoin.visibility = View.VISIBLE
+
+            //내 코인수와 비교했을 때 구매 불가능이면 빨간색 띄우기
+            var needCoin = borderCoin[i] - 10 //10에 내 코인 넣기
+            if (needCoin < 0){
+                binding.tvBWarningLabel.visibility = View.VISIBLE
+                binding.tvBNoticeLabel2.visibility = View.VISIBLE
+                binding.tvBWarningCoin.text = needCoin.toString()
+                binding.tvBWarningCoin.visibility = View.VISIBLE
+            }
+
+            binding.cvBBtn.strokeColor = getColor(R.color.BK)
+            binding.tvBBtn.text = "Release"
+        }
+        else {
+            binding.tvBNoticeLabel.visibility = View.GONE
+            binding.tvBNoticeCoin.visibility = View.GONE
+            binding.tvBNoticeLabel2.visibility = View.GONE
+
+            binding.tvBWarningLabel.visibility = View.GONE
+            binding.tvBNoticeLabel2.visibility = View.GONE
+            binding.tvBWarningCoin.visibility = View.GONE
+
+            binding.cvBBtn.strokeColor = getColor(R.color.BK)
+            binding.tvBBtn.text = "Select"
+        }
+    }
+
+    private fun borderChecking(i: Int) {
+        for (i in 0..5 step(1)){
+            icCheck[i].visibility = View.GONE
+            isChecked[i] = false
+        }
+
+        if (!isChecked[i]) {
+            icCheck[i].visibility = View.VISIBLE
+            binding.ifbBProfileBorder.setBackgroundColor(color[i])
+            isChecked[i] = true
+
+        } else {
+            icCheck[i].visibility = View.GONE
+            binding.ifbBProfileBorder.setBackgroundColor(getColor(R.color.WH))
+            isChecked[i] = false
+
+            binding.cvBBtn.strokeColor = getColor(R.color.GS_60)
+            binding.tvBBtn.text = "Select"
         }
     }
 }
